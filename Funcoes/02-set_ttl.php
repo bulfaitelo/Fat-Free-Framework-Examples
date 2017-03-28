654f958f8210f76d08ca50f0fa8502894c6f8566
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-03-28 09:25:32 -0300

    Pequenas correções em 03-set_ttl.php

--- Funcoes/02-set_ttl.php
+++ Funcoes/02-set_ttl.php
@@ -3,8 +3,9 @@
 // FRAMEWORK F3
 $f3 = require('../lib/base.php');
 
-// por padrão o cache fica desativado, entamos precisamos Ativar 
-$f3->set('CACHE',FALSE);
+// por padrão o cache fica desativado, entamos precisamos Ativar o/
+$f3->set('CACHE',TRUE);
+
 # lógica:
 #mixed set (String $key, mixed $val [, int $ttl = 0])
 
@@ -13,7 +14,7 @@ $f3->set('CACHE',FALSE);
 // Exmeplos variados de variaveis :)
 $f3->set('a',123, 3600); // a variavel a  = 123, salvas no cache por 1 hora
 $f3->set('b','c', 60); // a variavel b ='c'  (string), salvas no cache por 60 segundos
-$f3->set('c','qualqer coisa', 2); // a variavel c = 'qualquer coisa' (string), salvas no cache por 3 segundos
+$f3->set('c','qualqer coisa', 3); // a variavel c = 'qualquer coisa' (string), salvas no cache por 3 segundos
 
 // Imprimindo saidas 
 echo "<pre>";
@@ -21,26 +22,15 @@ echo "set 'a' saida=> ";
 var_dump($f3->get('a'));
 echo "set 'b' saida=> ";
 var_dump($f3->get('b'));
-echo "set 'c' saida=>  antes do sleep: \n";
-var_dump($f3->get('c'));
-echo "set 'c' saida=>  antes do sleep: \n";
+echo "set 'c' saida=>: \n";
 var_dump($f3->get('c'));
 echo "</pre>";
 
-// Exemplos de vetores 
-
-$f3->set('hash',array ('x'=> 1, 'y'=> 2, 'z'=> 3));
-
-//Támbém é possivel usar o ponto para criação de vetores
-$f3->set('vetor.x',1);
-$f3->set('vetor.y',2);
-$f3->set('vetor.x',3);
+// Exemplos de vetores
+$f3->set('hash',array ('x'=> 1, 'y'=> 2, 'z'=> 3), 3600); // Variavel armazenada em chache por 1 hora
 
 // Imprimindo saidas
 echo "<pre>";
 echo "set 'hash' saida=> ";
 var_dump($f3->get('hash'));
-echo "set 'vetor' saida => ";
-var_dump($f3->get('vetor'));
 echo "</pre>";
-
