fdbb085a450526a93e32d3a67b69b0ab098435f9
Bulfaitelo <bulfaitelo@gmail.com>
2017-03-29 17:47:28 -0300

    06-exists.php, 07-clear.php

--- Funcoes/05-exists.php
+++ Funcoes/05-exists.php
@@ -6,15 +6,14 @@ $f3 = require('../lib/base.php');
 # lógica:
 # bool exists ( string $key [, mixed &$val=NULL] )
 
-// verificando se uma variavel existe. 
-
+// verificando se uma variavel existe.
 $f3->set('foo','value');
 
 var_dump($f3->exists('foo')); // true
 var_dump($f3->exists('bar')); // false, porque a variavel não foi definida)
 
 // para funcionar tem que usar a função $f3->sync(); para sincronizar as as globais
-
-var_dump($f3->exists('COOKIE.userid')); // todas retornam false, não foram definidas antes.
-var_dump($f3->exists('SESSION.login')); // todas retornam false, não foram definidas antes.
-var_dump($f3->exists('POST.submit')); // todas retornam false, não foram definidas antes.
+var_dump($f3->exists('COOKIE.userid')); 
+var_dump($f3->exists('SESSION.login')); 
+var_dump($f3->exists('POST.submit')); 
+// todas retornam false, não foram definidas antes.
