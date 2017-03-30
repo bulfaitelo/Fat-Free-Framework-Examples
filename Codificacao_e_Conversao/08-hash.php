d129a94eaea190b73dbb749a73784d6ec903848d
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-03-30 17:39:01 -0300

    08-hash.php
    correção

--- Codificacao_e_Conversao/08-hash.php
+++ Codificacao_e_Conversao/08-hash.php
@@ -6,4 +6,8 @@ $f3 = require('../lib/base.php');
 # lógica:
 # string hash ( string $str )
 
-echo $f3->hash('Bulfaitelo'); //dyl3n4glpxrj
+echo $f3->hash('Bulfaitelo'); 
+
+// Saidas 
+// 'dyl3n4glpxrj' PHP 7.0.10
+// 'dyl3n4glpxs8' PHP 5.6.25
