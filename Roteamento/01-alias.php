b641c9416f6a73b351718386c40f2b0a47da002d
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-04-01 17:16:11 -0300

    01-alias.php

---
+++ Roteamento/01-alias.php
@@ -0,0 +1,12 @@
+<?php
+
+// FRAMEWORK F3
+$f3 = require('../lib/base.php');
+
+# lógica:
+# string alias ( string $name [, array|string $params = array() ] )
+
+$f3->route('GET @complex:/resize/@format/*/sep/*','App->nowhere');
+$f3->alias('complex','format=20x20,2=foo/bar,3=baz.gif');
+$f3->alias('complex',array('format'=>'20x20',2=>'foo/bar',3=>'baz.gif'));
+// ambos os exemplos retornam? '/resize/20x20/foo/bar/sep/baz.gif'
