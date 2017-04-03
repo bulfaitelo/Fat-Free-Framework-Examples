1a9e71c24723fd09b2ed463b69de1897d51de484
Thiago Rodrigues <bulfaitelo@gmail.com>
2017-04-03 15:34:47 -0300

    02-build.php

--- Roteamento/01-alias.php
+++ Roteamento/01-alias.php
@@ -9,4 +9,4 @@ $f3 = require('../lib/base.php');
 $f3->route('GET @complex:/resize/@format/*/sep/*','App->nowhere');
 $f3->alias('complex','format=20x20,2=foo/bar,3=baz.gif');
 $f3->alias('complex',array('format'=>'20x20',2=>'foo/bar',3=>'baz.gif'));
-// ambos os exemplos retornam? '/resize/20x20/foo/bar/sep/baz.gif'
+// Ambos os exemplos retornariam '/resize/20x20/foo/bar/sep/baz.gif'
---
+++ Roteamento/02-build.php
@@ -0,0 +1,28 @@
+<?php
+
+// FRAMEWORK F3
+$f3 = require('../lib/base.php');
+
+# lógica:
+# string build ( string $url [, array $params = array() ] )
+
+$f3->set('PARAMS.canal','Bullboladao');
+
+echo "<br>";
+echo $f3->build('@canal'); // exibe 'Bullboladao'
+echo "<br>";
+echo $f3->build('/get-it/now/@canal'); // exibe '/get-it/now/Bullboladao'
+echo "<br>";
+echo $f3->build('/subscribe/@canal');  // exibe '/subscribe/Bullboladao'
+echo "<br>";
+echo  $f3->build('/resize/@format/*/sep/*', array(
+	'format'=>'100x100',
+	2		=>'foo/bar',
+	3		=>'baz.gif'
+	));
+
+echo $f3->build('/resize/@format/*/sep/*',array(
+  'format'=>'200x200',// 1=>'200x200' também deferia funcionar. 
+  2=>'foo/bar',
+  3=>'baz.gif'
+));
